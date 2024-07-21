09765920be374a8f47780c98b47a9ff91ecec325
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-07-21 18:06:45 +0900

    refactor: split files

--- pkg/controller/add/add.go
+++ pkg/controller/add/add.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/spf13/afero"
-	"gopkg.in/yaml.v3"
 )
 
 type Param struct{}
@@ -50,26 +49,6 @@ func handleEntry(ctx context.Context, gh GitHub, _ *Config, _ *Entry) error {
 		if err := addItemToProject(ctx, gh, item); err != nil {
 			return fmt.Errorf("add an item to a project: %w", err)
 		}
-	}
-	return nil
-}
-
-func findConfig(fs afero.Fs) (afero.File, error) {
-	f, err := fs.Open("ghproj.yaml")
-	if err == nil {
-		return f, nil
-	}
-	return fs.Open(".ghproj.yaml") //nolint:wrapcheck
-}
-
-func findAndReadConfig(fs afero.Fs, cfg *Config) error {
-	f, err := findConfig(fs)
-	if err != nil {
-		return fmt.Errorf("find a configuration file: %w", err)
-	}
-	defer f.Close()
-	if err := yaml.NewDecoder(f).Decode(cfg); err != nil {
-		return fmt.Errorf("decode a configuration file: %w", err)
 	}
 	return nil
 }
