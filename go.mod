e61d252b3306d3299c0e5e8930472ee857c9249c
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-07-21 17:33:50 +0900

    feat: initial setup (#3)
    * feat: initial setup
    
    * chore: add renovate.json5 and scripts
    
    * fix: fix a lint error
    
    * ci: remove a broken test

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/suzuki-shunsuke/ghproj
+
+go 1.22.5
+
+require (
+	github.com/mattn/go-colorable v0.1.13
+	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/afero v1.11.0
+	github.com/suzuki-shunsuke/logrus-error v0.1.4
+	github.com/urfave/cli/v2 v2.27.2
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ pkg/cli/add.go
@@ -0,0 +1,36 @@
+package cli
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
+	"github.com/suzuki-shunsuke/ghproj/pkg/controller/add"
+	"github.com/suzuki-shunsuke/ghproj/pkg/log"
+	"github.com/urfave/cli/v2"
+)
+
+type addCommand struct {
+	logE *logrus.Entry
+}
+
+func (rc *addCommand) command() *cli.Command {
+	return &cli.Command{
+		Name:      "add",
+		Usage:     "Add GitHub Issues and Pull Requests to GitHub Projects",
+		UsageText: "ghproj set",
+		Description: `Add GitHub Issues and Pull Requests to GitHub Projects.
+
+$ ghproj add
+`,
+		Action: rc.action,
+		Flags:  []cli.Flag{},
+	}
+}
+
+func (rc *addCommand) action(c *cli.Context) error {
+	fs := afero.NewOsFs()
+	logE := rc.logE
+	log.SetLevel(c.String("log-level"), logE)
+	log.SetColor(c.String("log-color"), logE)
+	return add.Add(c.Context, fs, &add.Param{ //nolint:wrapcheck
+	})
+}
