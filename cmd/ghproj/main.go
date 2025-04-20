37832fd11a406a589b307839d09c661ca6af036b
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-04-20 14:10:32 +0900

    refactor: use suzuki-shunsuke/urfave-cli-v3-util/{urfave,log} (#196)
    * refactor: use suzuki-shunsuke/urfave-cli-v3-util/{urfave,log}
    
    * refactor: remove cli.Runner

--- cmd/ghproj/main.go
+++ cmd/ghproj/main.go
@@ -8,8 +8,9 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/suzuki-shunsuke/ghproj/pkg/cli"
-	"github.com/suzuki-shunsuke/ghproj/pkg/log"
 	"github.com/suzuki-shunsuke/logrus-error/logerr"
+	"github.com/suzuki-shunsuke/urfave-cli-v3-util/log"
+	"github.com/suzuki-shunsuke/urfave-cli-v3-util/urfave"
 )
 
 var (
@@ -19,25 +20,24 @@ var (
 )
 
 func main() {
-	logE := log.New(version)
+	logE := log.New("ghproj", version)
 	if err := core(logE); err != nil {
 		logerr.WithError(logE, err).Fatal("ghproj failed")
 	}
 }
 
 func core(logE *logrus.Entry) error {
-	runner := cli.Runner{
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	return cli.Run(ctx, &urfave.Runner{ //nolint:wrapcheck
 		Stdin:  os.Stdin,
 		Stdout: os.Stdout,
 		Stderr: os.Stderr,
-		LDFlags: &cli.LDFlags{
+		LDFlags: &urfave.LDFlags{
 			Version: version,
 			Commit:  commit,
 			Date:    date,
 		},
 		LogE: logE,
-	}
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-	return runner.Run(ctx, os.Args...) //nolint:wrapcheck
+	}, os.Args...)
 }
--- pkg/cli/runner.go
+++ pkg/cli/runner.go
@@ -2,33 +2,16 @@ package cli
 
 import (
 	"context"
-	"io"
 
-	"github.com/sirupsen/logrus"
-	"github.com/suzuki-shunsuke/urfave-cli-v3-util/helpall"
-	"github.com/suzuki-shunsuke/urfave-cli-v3-util/vcmd"
+	"github.com/suzuki-shunsuke/urfave-cli-v3-util/urfave"
 	"github.com/urfave/cli/v3"
 )
 
-type Runner struct {
-	Stdin   io.Reader
-	Stdout  io.Writer
-	Stderr  io.Writer
-	LDFlags *LDFlags
-	LogE    *logrus.Entry
-}
-
-type LDFlags struct {
-	Version string
-	Commit  string
-	Date    string
-}
-
-func (r *Runner) Run(ctx context.Context, args ...string) error {
-	return helpall.With(&cli.Command{ //nolint:wrapcheck
+func Run(ctx context.Context, runner *urfave.Runner, args ...string) error {
+	return runner.Command(&cli.Command{ //nolint:wrapcheck
 		Name:    "ghproj",
 		Usage:   "",
-		Version: r.LDFlags.Version + " (" + r.LDFlags.Commit + ")",
+		Version: runner.LDFlags.Version,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:  "log-level",
@@ -42,20 +25,15 @@ func (r *Runner) Run(ctx context.Context, args ...string) error {
 		EnableShellCompletion: true,
 		Commands: []*cli.Command{
 			(&initCommand{
-				logE: r.LogE,
+				logE: runner.LogE,
 			}).command(),
 			(&addCommand{
-				logE: r.LogE,
+				logE: runner.LogE,
 			}).command(),
 			(&completionCommand{
-				logE:   r.LogE,
-				stdout: r.Stdout,
+				logE:   runner.LogE,
+				stdout: runner.Stdout,
 			}).command(),
-			vcmd.New(&vcmd.Command{
-				Name:    "ghproj",
-				Version: r.LDFlags.Version,
-				SHA:     r.LDFlags.Commit,
-			}),
 		},
-	}, nil).Run(ctx, args)
+	}).Run(ctx, args)
 }
